e2aa4b7eca8721a076c96b9d1a798787a005106a
milagrospalma <mnpalma.2@gmail.com>
2018-03-13 14:13:25 -0500

    actualiza componentes componibles

---
+++ src/components/HobbyItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HobbyItem = ({hobby}) => (
+  <li>{hobby}</li>
+)
+
+export default HobbyItem;
--- src/components/HobbyList.js
+++ src/components/HobbyList.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import HobbyItem from './HobbyItem'
+
+const hobbies = [
+  { id: 1, hobby: 'cantar'},
+  { id: 2, hobby: 'comer' },
+  { id: 3, hobby: 'escribir' }
+]
 
 const HobbyList = () => (
   <ul>
-    <li>Comer</li>
-    <li>Cantar</li>
-    <li>Escribir</li>
+    {hobbies.map(hobby =>
+      <HobbyItem key={hobbies.id}
+        {...hobby}
+      />
+    )}
   </ul>
 )
 
